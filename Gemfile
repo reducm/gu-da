6c61a423cf092bed7b7d8263b02019b377bdabb0
JAS <reducm@gmail.com>
2012-02-26 16:13:10 +0800

    first commit

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'http://ruby.taobao.org'
+
+gem 'rails', '3.1.3'
+
+#数据
+gem 'mysql2'
+
+#禁用asset日志
+gem 'quiet_assets', :git => 'git://github.com/AgilionApps/quiet_assets.git'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.1.5'
+  gem 'coffee-rails', '~> 3.1.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+group :development, :test do
+    gem 'rspec-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GuDa::Application.config.secret_token = '8976bad52ca223bc2e850ebdfe99b32012dad0c7d20ff91292a628ca258c0ea01d680771bb989d6a2605e34b136f2708c5510752bd6a1c425d2ecce639c0d4fb'
