b173d49f9801031bd74131f499e5d342c6ccbcfe
reducm <reducm@gmail.com>
2012-03-31 00:41:51 +0800

    修复了user编码的问题

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,7 +48,6 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-
     respond_to do |format|
       if @user.update_attributes(params[:user])
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-# encodin: UTF-8
+# encoding: UTF-8
 class User < ActiveRecord::Base
     has_many :articles
     has_many :replies
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
     attr_accessible :name, :email, :password, :password_confirm, :head, :birthday, :description, :habbit
     validates_uniqueness_of :name, :email
 
-    validates :name, :presence => {:message => '用户名不能为空'}, :uniqueness => {:message => '用户名已存在' } 
+    validates :name, :presence => { :message => '用户名不能为空' }, :uniqueness => {:message => '用户名已存在' } 
     validates :email, :presence => {:message => 'Email不能为空'}, :uniqueness => {:message => 'Email已存在' },
                       :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create, :message => 'Email格式不正确' }
     validates :password, :presence => {:message => '密码不能为空'} 
