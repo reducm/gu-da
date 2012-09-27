22714b289aedbdfc6796ba2395f16cdcd949101c
jojo <reducm@gmail.com>
2012-09-28 01:43:52 +0800

    working on pic.js

--- app/assets/javascripts/pic/pic.js.coffee
+++ app/assets/javascripts/pic/pic.js.coffee
@@ -1,3 +1,14 @@
 $(document).ready(->
-
+  $('body').bind("dragover", ()->
+    e.stopPropagation()
+    e.preventDefault()
+    false
+  )
+  $("#drag_test").draggable()
+  $("#drop_file").droppable(
+    {
+      drop:(event,ui)->
+        $(this).html(ui.draggable.html())
+    }
+  )
 )
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,9 @@
+#encoding: UTF-8
 class PagesController < ApplicationController
   before_filter :check_login
   before_filter :check_session
   before_filter :check_admin
-  
+# TODO: 暂时先手动添加jquery ui, 以后要自己把jqueryui做成gem模式并且可以自定义载入模块 
   def update
     page = params[:page]
     if page[:title] == 'index_images' && page[:pictures]
